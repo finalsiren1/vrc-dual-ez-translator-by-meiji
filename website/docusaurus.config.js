cb19419ea4e2878213aaa8b30db8377c8b966306
finalsiren1 <13belovedd@gmail.com>
2026-05-12 01:01:19 +0700

    update

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -10,8 +10,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'SyncVRC',
+  tagline: 'Real-time Voice Translation for VRChat',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
@@ -148,7 +148,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} SyncVRC by Meiji.`,
       },
       prism: {
         theme: prismThemes.github,
